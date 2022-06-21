debe7ea02895579a33d678ae3b16b4d3831fd81b
Nurbol Abaikanov <Abaikanov@Nurbols-MacBook-Pro.local>
2022-06-21 17:07:30 +0600

    Initial commit

---
+++ src/com/company/Triangle.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Triangle {
+
+    int a=5 ;
+    int b=5;
+    int c=5;
+    int h=3;
+
+        void area(){
+            System.out.println("Area of Triangle is : "+(a*h)/2);
+
+    }
+
+}
